831455e3dff6268abec2d9531811583e6e5ef24d
Theo Henrique <theodoro222@hotmail.com>
2025-04-17 22:59:42 -0300

    config App.js and navbar

--- resources/js/pages/App.vue
+++ resources/js/pages/App.vue
@@ -1,13 +1,21 @@
 <script lang="ts">
 import { Head, Link } from '@inertiajs/vue3';
+import Home from '@/pages/templates/Home.vue';
+import Navbar from './components/Navbar.vue';
 export default {
     components: {
         Head,
         Link,
+        Home,
+        Navbar,
     }
 }
 </script>
 
 <template>
-    <Head title="Index"/>
+    <Head title="Nutribase"/>
+    <div>
+        <Navbar/>
+        <Home/>
+    </div>
 </template>
