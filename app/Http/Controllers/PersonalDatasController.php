c95540035616f4491756e762eef9378d1b6778fe
Theo Henrique <theodoro222@hotmail.com>
2025-04-23 01:05:39 -0300

    create model and alteration controller

--- app/Http/Controllers/PersonalDatasController.php
+++ app/Http/Controllers/PersonalDatasController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\PersonalData;
 use Illuminate\Http\Request;
 
-class PersonalDatas extends Controller
+class PersonalDatasController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -34,7 +33,7 @@ class PersonalDatas extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(PersonalData $personalData)
+    public function show(string $id)
     {
         //
     }
@@ -42,7 +41,7 @@ class PersonalDatas extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(PersonalData $personalData)
+    public function edit(string $id)
     {
         //
     }
@@ -50,7 +49,7 @@ class PersonalDatas extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, PersonalData $personalData)
+    public function update(Request $request, string $id)
     {
         //
     }
@@ -58,7 +57,7 @@ class PersonalDatas extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(PersonalData $personalData)
+    public function destroy(string $id)
     {
         //
     }
--- app/Models/PersonalData.php
+++ app/Models/PersonalData.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PersonalData extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'age',
+        'height',
+        'weight',
+        'arm_l',
+        'arm_r',
+        'chest',
+        'waist',
+        'scruff',
+        'thigh_l',
+        'thigh_r',
+        'calf',
+    ];
 }
