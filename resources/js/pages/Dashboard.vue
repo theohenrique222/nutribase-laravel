11c5e5805b365d9bc3347dd0a803ff012c1503d6
Theo Henrique <theodoro222@hotmail.com>
2025-04-28 20:58:34 -0300

    alteration background color dashboard

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -6,7 +6,7 @@ import PlaceholderPattern from '../components/PlaceholderPattern.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: 'Dashboard',
+        title: 'Painel de controle',
         href: '/dashboard',
     },
 ];
@@ -16,7 +16,7 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Painel de Controle"/>
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+        <div class="bg-neutral-900 flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <!-- <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
