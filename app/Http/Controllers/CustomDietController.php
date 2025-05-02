62c5a15b7127e8231e7a17126e422a5b830e24e8
Theo Henrique <theodoro222@hotmail.com>
2025-05-02 19:43:16 -0300

    create mcr from custom diet

---
+++ app/Http/Controllers/CustomDietController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CustomDiet;
+use Illuminate\Http\Request;
+
+class CustomDietController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CustomDiet $customDiet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CustomDiet $customDiet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CustomDiet $customDiet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CustomDiet $customDiet)
+    {
+        //
+    }
+}
