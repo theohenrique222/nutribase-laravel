19cfd260a8cd101ddb209d8c16344eaa2923ad75
theohenrique222 <theodoro222@hotmail.com>
2025-11-15 19:56:00 -0300

    correction view index from products

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -2,13 +2,13 @@
 @section('title', 'Produtos')
 @section('content')
 
-    <section class="pt-16 min-h-screen bg-neutral-800">
+    <section class="min-h-screen pt-16 bg-neutral-800">
         <div class="text-center mt-10 mb-8">
             <h1 class="text-3xl font-bold text-lime-500">Produtos Cadastrados</h1>
             <p class="text-gray-50 font-light">Acompanhe e gerencie os produtos utilizados nas suas dietas</p>
         </div>
 
-        <div class="bg-white shadow-xl rounded-2xl overflow-hidden max-w-6xl mx-auto">
+        <div class="bg-gray-100 min-h-80 backdrop-blur-sm shadow-md rounded-xl overflow-hidden max-w-6xl m-auto">
             <div class="grid grid-cols-4 bg-lime-500 text-neutral-50 font-semibold uppercase text-center p-4 border-b">
                 <p>Nome</p>
                 <p>Unidade</p>
@@ -17,7 +17,7 @@
             </div>
 
             @forelse ($products as $product)
-                <div class="grid grid-cols-1 sm:grid-cols-4 items-center text-center border-b p-4 hover:bg-gray-50 transition">
+                <div class="grid grid-cols-4 text-center items-center border-b border-neutral-300 p-4 bg-white hover:bg-lime-50 transition">
                     <p class="font-medium text-gray-900">{{ $product->name }}</p>
                     <p class="text-gray-700">{{ $product->portion_unit }}</p>
                     <p class="text-gray-700">{{ $product->observation }}</p>
@@ -44,9 +44,9 @@
                     <p>Você ainda não possui produtos cadastrados.</p>
                 </div>
             @endforelse
-            <div class="m-auto text-center p-3">
+            <div class="text-center mt-10">
                 <a href="{{ route('products.create') }}"
-                    class="inline-block mt-4 bg-lime-500 hover:bg-lime-600 text-white font-medium px-4 py-2 rounded-lg transition">
+                    class="inline-block mt-4 bg-lime-500 hover:bg-lime-600 text-white font-medium px-4 py-2 rounded-md transition">
                     + Cadastrar Produto
                 </a>
             </div>
