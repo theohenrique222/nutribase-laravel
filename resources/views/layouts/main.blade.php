6f989e226897f5e9b5f6d10357e013786d9f28c4
theohenrique222 <theodoro222@hotmail.com>
2025-11-17 12:00:17 -0300

    add alpine js in project from cdn

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,14 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite('resources/css/app.css')
-    {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
     <title>{{ config('app.name') }} | @yield('title')</title>
-    <!-- Fonts -->
-    {{--
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-    --}}
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
@@ -25,6 +18,7 @@
     <main>
         @yield('content')
     </main>
+    <script src="//unpkg.com/alpinejs" defer></script>
 </body>
 
 </html>
