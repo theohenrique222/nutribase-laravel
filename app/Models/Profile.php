bf8f7fe933c9877e017c2b98f0febf30389aba38
theohenrique222 <theodoro222@hotmail.com>
2026-03-14 15:13:54 -0300

    corretion rlation profile as user

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -17,7 +17,7 @@ class Profile extends Model
 
     public function user()
     {
-        $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function history(): HasMany
